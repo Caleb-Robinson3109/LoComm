c8de8182f0669be1021fee842a8a8d4d178eb3f8
ejkmvp@umsystem.edu <ejkmvp@umsystem.edu>
2025-09-29 11:57:03 -0500

    added sample file

---
+++ src/sample/sample/sample.ino
@@ -0,0 +1,106 @@
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+//Libraries for OLED Display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+//Frequency Band
+#define BAND 915E6
+
+//OLED pins
+#define OLED_SDA 4
+#define OLED_SCL 15 
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+//packet counter
+int counter = 0;
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+void setup() {
+  //initialize Serial Monitor
+  Serial.begin(115200);
+
+  //reset OLED display via software
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+
+  //initialize OLED
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32 //TODO figure out what the false parameters do
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  
+  //initialize OLED
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+
+  //Initialize LoRa device
+  SPI.begin(SCK, MISO, MOSI, SS);
+  //setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+  
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  Serial.println("LoRa Initializing OK!");
+  display.setCursor(0,10);
+  display.print("LoRa Initializing OK!");
+  display.display();
+  delay(2000);
+}
+
+//Notes for programming...
+
+//Display - 
+  //set cursor is used to place the cursor from the top left of the screen
+  //display.setCursor(0,0);
+
+  //Print is used to print text
+  //display.print("LORA SENDER ");
+
+  //Display is used to push the buffer to the screen itself
+  //display.display();
+
+//LoRa
+  //Start a packet
+  //LoRa.beginPacket();
+
+  //Add data to the packet
+  //LoRa.print("")
+  
+  //End a packet and send it out
+  //LoRa.endPacket()
+
+  //Try to receive a packet
+  //LoRa.parsePacket(); //will return the length of the received packet. if its zero, then no packet was received
+  
+  //LoRa.available(); //true if a byte is available
+  //LoRa.read(); //pop a byte of the packet
+  //LoRa.packetRssi(); //read the rssi of the packet 
+
+//
+
+
+void loop() {
+  
+  delay(10000);
+}
